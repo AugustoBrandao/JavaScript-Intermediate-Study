e636eb26338c69891a9aa0852de9a6739fae07e1
Augusto Brandão <37248840+AugustoBrandao@users.noreply.github.com>
2022-06-12 19:32:21 -0300

    .

---
+++ ex12-Bhaskara/delta.js
@@ -0,0 +1,3 @@
+export default function delta(a,b,c){
+    return (b**2)-4*a*c
+}
---
+++ ex12-Bhaskara/xFunctions.js
@@ -0,0 +1,7 @@
+export function functionX1(a,b,d){
+    return (-b + Math.sqrt(d)) / 2 * a
+}
+
+export function functionX2(a,b,d){
+    return (-b - Math.sqrt(d)) / 2 * a
+}
